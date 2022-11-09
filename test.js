f2bc05a0bfe0352d4ff698e25a2bf894e5b7bee8
RyanTjia19 <ryan.tjia19@my.stjohns.edu>
2022-11-09 12:59:38 -0500

    *Added a folder to contain any new files (or modules) *Added a new file to store the routes

--- test.js
+++ test.js
@@ -6,10 +6,10 @@ const express = require('express');
 const app = express();
 
 //Node treats every file as a module, but they must be in the "node_modules" folder
-var math = require('testModule.js');
+var math = require('customModules/mathModule.js');
+var externalRoutes = require('customModules/routes.js');
 
 //Defines a route, 'req' or first parameter is request, 'res' or second parameter is response
-
 //Url is "localhost:3000"
 app.get('/', function(req, res) {
 	res.send('<h1>Hello!</h1><p>The response given must be on the same line</p>');
@@ -50,18 +50,11 @@ app.get('/info/:data', function(req, res) {
 
 //app.post() is used for when the user submit data via form
 //app.get() is used when the user clicks on a link
-app.post('areaCircle', (req, res) => {
-	var radius = req.body.radius;
-	var area = math.circle_Area(radius);
-	res.send(`<h1>An area of a circle</h1><p>Given a circle with a radius of ${radius}, it will have an area of</p><p>${area}</p>`);
-})
-
-app.get('/areaCircle/:radius', (req, res) => {
-	var radius = req.params['radius'];
-
-	var area = math.circle_Area(radius);
-
-	res.send(`<h1>An area of a circle</h1><p>Given a circle with a radius of ${radius}, it will have an area of</p><p>${area}</p>`);
+app.post('/areaCircle', (req, res) => {
+	res.send('<h1>This works</h1>');
+	//var radius = Number(req.body.input);
+	//var area = math.circle_Area(radius);
+	//res.send(`<h1>An area of a circle</h1><p>Given a circle with a radius of ${radius}, it will have an area of</p><p>${area}</p>`);
 })
 
 app.get('/volumeBox/:length/:width/:height', (req, res) => {
@@ -69,10 +62,13 @@ app.get('/volumeBox/:length/:width/:height', (req, res) => {
 	var width = req.params['width'];
 	var height = req.params['height'];
 
-	var volume = math.box_Volume(length, width, height);
+	var volume = math.boxVolume(length, width, height);
 
 	res.send(`<h1>The volume of a rectangular prism</h1><p>Given a rectangular prism with a length of ${length}, width of ${width}, and height of ${height}, it will have a volume of</p><p>${volume}</p>`);
 })
+
+//Using the routes from the external file
+app.use('/external', externalRoutes);
 
 //Starts a server -- Do 'node test.js' to run
 //First parameter is the port number
